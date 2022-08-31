3b2f07933c1ba32e3dc17af85e67a6460c453e86
asadhijj <hijjawi30@gmail.com>
2022-09-01 02:49:00 +0300

    server is up

---
+++ Movies.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+
+async function getMovies(req,res) {
+    const searchQuery = req.query.searchQuery;
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY_MOVIES}&query=${searchQuery}&page=1`;
+  
+    axios.get(URL).then( result => {
+        let sendData = result.data.results.map( file => {
+            return new Movies(file);
+        })
+        return res.status(200).send(sendData);
+    }).catch(error => {
+        return res.status(404).send(error)
+    })
+  }
+  
+class Movies {
+    constructor(item){
+        this.title = item.title;
+        this.overview = item.overview;
+        this.vote_average = item.vote_average;
+        this.vote_count = item.vote_count;
+        this.poster_path = "https://image.tmdb.org/t/p/w500/" + item.poster_path;
+        this.popularity = item.popularity;
+        this.release_date = item.release_date;
+        
+    }
+}
+
+
+
+
+module.exports= getMovies;
--- server.js
+++ server.js
@@ -2,33 +2,21 @@ require("dotenv").config();
 const express = require("express"); //import express framework
 const cors = require("cors");
 const server = express();
-const weatherDat = require("./assets/weather.json");
-const Forecast = require("./Forecast");
+const getWeather=require('./Forecast')
+const getMovies =require ('./Movies')
 server.use(cors());
 const PORT = process.env.PORT;
 
-// http://localhost:4000/weather?ser
-server.get("/weather", (req, res) => {
-  const searchQuery = req.query.searchQuery || "";
 
-  const response = [];
+//http://localhost:PORT/weather?lon=lon&lat=lat
+server.get('/weather', getWeather);
 
-  const cityData = weatherDat.find((e) => e["city_name"] === searchQuery);
+//http://localhost:PORT/Movies?searchQuery=searchQuery
+server.get('/Movies', getMovies)
 
-  if (cityData) {
-    cityData?.data.forEach((city) => {
-      response.push(
-        new Forecast(
-          city["valid_date"],
-          `Low of ${city["low_temp"]}, high of ${city["max_temp"]}, ${city.weather.description}`
-        )
-      );
-    });
-    res.json({ forecast: response, lon: cityData?.lon, lat: cityData?.lat });
-  } else {
-    res.sendStatus(404)
-  }
-});
+
+
+
 
 server.listen(PORT || 4000, () => {
   console.log(`Hello, I am listening on ${PORT}`);
